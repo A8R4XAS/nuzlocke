a382a736c792d4ffc238698a4c78e5d89cd6513e
jenni01s <schmidtjennifer01@gmail.com>
2021-05-22 13:32:22 +0200

    added System.Random
    Co-authored-by: A8R4XAS <A8R4XAS@users.noreply.github.com>

--- processing.hs
+++ processing.hs
@@ -1,7 +1,12 @@
+import System.Random
 {-
 random regel ausgabe
 hi
 -}
 
 regeln :: [ String ]
-regeln = ["Permadeath", " Nur das erste Pokemon pro Route darf gefangen werden", "Spietznamen sind pflicht"]
+regeln = ["Permadeath", "Nur das erste Pokemon pro Route darf gefangen werden", "Spietznamen sind Pflicht",
+          "Thema-Team","Pro Typ maximal ein Pokemon, erster Typ zaehlt" ,"Keine Flucht vor wilden Pokemon", 
+          "Routen nur zu Fuß verlassen", "keine Items verwenden", "nächster Arenakampf ohne Gehaltene Items",
+          "keine Z-Attacken oder Mega-Entwicklung beim Arenakampf"]
+
